62d7992a76ae34aa8c3a151520088585b7cb33b6
Losia <1064972820@qq.com>
2020-10-03 18:14:25 +0800

    在主界面添加点击重量按钮的响应处理，跳出弹窗并显示提示信息

--- mywork_3/app/src/main/java/com/example/mywork_3/SecondActivity.java
+++ mywork_3/app/src/main/java/com/example/mywork_3/SecondActivity.java
@@ -2,13 +2,35 @@ package com.example.mywork_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.media.tv.TvContract;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button2:
+                ProgressDialog progressDialog = new ProgressDialog(SecondActivity.this);
+                progressDialog.setTitle("即将展现体重数据");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
+                break;
+            default:
+                break;
+        }
     }
 }
