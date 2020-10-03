57d8a665aecbef808158f1312b8b6cc05e725f81
Losia <1064972820@qq.com>
2020-10-03 08:53:01 +0800

    重新更改

--- mywork_2/app/src/main/java/com/example/work_2/MainActivity.java
+++ mywork_2/app/src/main/java/com/example/work_2/MainActivity.java
@@ -19,37 +19,5 @@ public class MainActivity extends AppCompatActivity {
         //设置标题
         this.setTitle("MainActivity");
 
-        //创建四个按钮并设置每种按钮的跳转界面
-        Button button1 = (Button) findViewById(R.id.button);
-        Button button2 = (Button) findViewById(R.id.button2);
-        Button button3 = (Button) findViewById(R.id.button3);
-        Button button4 = (Button) findViewById(R.id.button4);
-        button1.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this,Second.class);
-                startActivity(intent);
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this,Thrid.class);
-                startActivity(intent);
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this,Fourth.class);
-                startActivity(intent);
-            }
-        });
     }
 }
