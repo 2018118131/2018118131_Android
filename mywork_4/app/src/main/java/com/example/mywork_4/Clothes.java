474ffbc65854e5c0f63536ece4035fac0c508d94
Losia <1064972820@qq.com>
2020-10-05 21:47:22 +0800

    在Clothes类创建图片属性、构造方法、返回方法

--- mywork_4/app/src/main/java/com/example/mywork_4/Clothes.java
+++ mywork_4/app/src/main/java/com/example/mywork_4/Clothes.java
@@ -2,4 +2,20 @@ package com.example.mywork_4;
 
 public class Clothes {
 
+    private String name;
+    private int imageId;
+
+    public Clothes(String name,int imageId){
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getImageId(){
+        return  imageId;
+    }
+
 }
