2050ea9652490eca563155b15888629a145d687e
Losia <1064972820@qq.com>
2020-10-05 22:39:51 +0800

    RecyclerView的点击事件

--- mywork_4/app/src/main/java/com/example/mywork_4/ClothesAdapter.java
+++ mywork_4/app/src/main/java/com/example/mywork_4/ClothesAdapter.java
@@ -6,8 +6,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
@@ -18,9 +16,11 @@ public class ClothesAdapter extends RecyclerView.Adapter<ClothesAdapter.ViewHold
     static class ViewHolder extends RecyclerView.ViewHolder{
         ImageView clothesImage;
         TextView clothesName;
+        View clothesView;
 
         public ViewHolder(View view){
             super(view);
+            clothesView = view;
             clothesImage = (ImageView) view.findViewById(R.id.clothes_image);
             clothesName = (TextView) view.findViewById(R.id.clothes_name);
         }
@@ -33,7 +33,22 @@ public class ClothesAdapter extends RecyclerView.Adapter<ClothesAdapter.ViewHold
     @Override
     public ClothesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.clothes_item,parent,false);
-        ViewHolder holder = new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.clothesView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                int position = holder.getAdapterPosition();
+                Clothes clothes = mClothesList.get(position);
+                Toast.makeText(v.getContext(),"你点击了子项" + clothes.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.clothesImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Clothes clothes = mClothesList.get(position);
+                Toast.makeText(v.getContext(),"你点击了图片" + clothes.getName(),Toast.LENGTH_SHORT ).show();
+            }
+        });
         return holder;
     }
 
