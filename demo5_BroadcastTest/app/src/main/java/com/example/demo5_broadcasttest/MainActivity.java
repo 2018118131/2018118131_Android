3b007dbaa663be6783a34ef222524038d2f37431
Losia <1064972820@qq.com>
2020-11-11 21:46:24 +0800

    实现监听网络变化

--- demo5_BroadcastTest/app/src/main/java/com/example/demo5_broadcasttest/MainActivity.java
+++ demo5_BroadcastTest/app/src/main/java/com/example/demo5_broadcasttest/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.demo5_broadcasttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver,intentFilter);
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context,"newwork changes",Toast.LENGTH_SHORT).show();
+        }
     }
 }
