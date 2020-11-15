7f3c8809c2cf40a8e07ee4ef7dc05aefb24dbb7f
Losia <1064972820@qq.com>
2020-11-15 21:51:41 +0800

    创建BaseActivity类作为所有类的父类

---
+++ mywork_6/app/src/main/java/com/example/mywork_6/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.example.mywork_6;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+}
