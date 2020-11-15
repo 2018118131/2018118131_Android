4cfffc2d25a5323a904bc629a0bcf246e901fd4a
Losia <1064972820@qq.com>
2020-11-15 22:17:14 +0800

    把广播注册放在onResume()中

--- mywork_6/app/src/main/java/com/example/mywork_6/BaseActivity.java
+++ mywork_6/app/src/main/java/com/example/mywork_6/BaseActivity.java
@@ -1,10 +1,13 @@
 package com.example.mywork_6;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+
+    private ForceOfflineReceiver receiver;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -14,6 +17,14 @@ public class BaseActivity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         ActivityCollector.removeActivity(this);
+    }
+
+    protected void onResume(){
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.mywork_6.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver,intentFilter);
     }
 
 }
