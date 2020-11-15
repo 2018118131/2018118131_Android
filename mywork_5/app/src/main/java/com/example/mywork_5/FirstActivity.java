f5045cdde312a178f027c99b19c9604b65c261ff
Losia <1064972820@qq.com>
2020-11-15 19:31:46 +0800

    创建活动

---
+++ mywork_5/app/src/main/java/com/example/mywork_5/FirstActivity.java
@@ -0,0 +1,13 @@
+package com.example.mywork_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
