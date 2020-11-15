5f390c3acb5596b12f6441c5c28155f734e52216
Losia <1064972820@qq.com>
2020-11-15 22:23:26 +0800

    创建ForceOfflineReceiver()广播接收器

--- mywork_6/app/src/main/java/com/example/mywork_6/BaseActivity.java
+++ mywork_6/app/src/main/java/com/example/mywork_6/BaseActivity.java
@@ -1,8 +1,13 @@
 package com.example.mywork_6;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
@@ -25,6 +30,34 @@ public class BaseActivity extends AppCompatActivity {
         intentFilter.addAction("com.example.mywork_6.FORCE_OFFLINE");
         receiver = new ForceOfflineReceiver();
         registerReceiver(receiver,intentFilter);
+    }
+
+    protected void onPause(){
+        super.onPause();
+        if(receiver!=null){
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
+    class ForceOfflineReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You are forced to be offline.Please try to login again.");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int which) {
+                    ActivityCollector.finishAll();
+                    Intent intent = new Intent(context,LoginActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+            builder.show();
+        }
     }
 
 }
