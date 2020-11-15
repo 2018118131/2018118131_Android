a3f916d5fdb3cb0665f3c3eb848af002bb685e1d
Losia <1064972820@qq.com>
2020-11-15 19:53:41 +0800

    重写onOptionsItemSelected()方法给每个菜单项加入自己的逻辑处理

--- mywork_5/app/src/main/java/com/example/mywork_5/FirstActivity.java
+++ mywork_5/app/src/main/java/com/example/mywork_5/FirstActivity.java
@@ -1,8 +1,11 @@
 package com.example.mywork_5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,5 +22,25 @@ public class FirstActivity extends AppCompatActivity {
                 Toast.makeText(FirstActivity.this,"You clicked Button 1",Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
