d2d2faa86294a952b8a6a5977a6619241acff89f
Losia <1064972820@qq.com>
2020-12-04 22:02:57 +0800

    注册控件并添加事件监听

--- mywork_9/app/src/main/java/com/example/mywork_9/MainActivity.java
+++ mywork_9/app/src/main/java/com/example/mywork_9/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.mywork_9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text);
+        Button startService = (Button) findViewById(R.id.start_service);
+        Button stopService = (Button) findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
