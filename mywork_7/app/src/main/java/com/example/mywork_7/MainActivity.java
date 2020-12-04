4170ea828c91cf28052f5a8b3c546295912acbc4
Losia <1064972820@qq.com>
2020-12-04 08:16:52 +0800

    定义一个线程通信的方法，用于在控制台显示通信信息

--- mywork_7/app/src/main/java/com/example/mywork_7/MainActivity.java
+++ mywork_7/app/src/main/java/com/example/mywork_7/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.mywork_7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "线程通信";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void threadExchangeData(Handler handler){
+        Message msg = Message.obtain();
+        String currThreadName = Thread.currentThread().getName();
+        int data = new Random().nextInt();
+        msg.obj = data;
+        Log.d(TAG,"["+currThreadName+"] 发出随机数" +data);
+        handler.sendMessage(msg);
+    }
+
 }
