8d46c8199ec6a256ed8dc52793b98ad7b1c7a3f8
Losia <1064972820@qq.com>
2020-12-05 10:49:44 +0800

    修改部分错误代码

--- mywork_10/app/src/main/java/com/example/mywork_10/DownloadService.java
+++ mywork_10/app/src/main/java/com/example/mywork_10/DownloadService.java
@@ -70,8 +70,7 @@ public class DownloadService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 
     class DownloadBinder extends Binder {
