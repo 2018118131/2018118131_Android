908eaf5cb567cf1b9e81386c9652d33534c68f54
Losia <1064972820@qq.com>
2020-12-05 21:09:31 +0800

    为悬浮按钮添加事件处理

--- mywork_11/app/src/main/java/com/example/mywork_11/MainActivity.java
+++ mywork_11/app/src/main/java/com/example/mywork_11/MainActivity.java
@@ -11,8 +11,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 
@@ -39,6 +41,12 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 mDrawerLayout.closeDrawers();
                 return true;
+            }
+        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"FAB clicked",Toast.LENGTH_SHORT).show();
             }
         });
 
