f000493905a749c24d860e000fb827ae0e311fb1
Losia <1064972820@qq.com>
2020-12-05 10:29:38 +0800

    对各个按钮进行初始化操作并设置点击事件

--- mywork_10/app/src/main/java/com/example/mywork_10/MainActivity.java
+++ mywork_10/app/src/main/java/com/example/mywork_10/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -28,6 +29,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startDownload = (Button) findViewById(R.id.start_download);
+        Button pauseDownload = (Button) findViewById(R.id.pause_download);
+        Button cancelDownload = (Button) findViewById(R.id.cancel_download);
+        startDownload.setOnClickListener(this);
+        pauseDownload.setOnClickListener(this);
+        cancelDownload.setOnClickListener(this);
     }
 
     @Override
