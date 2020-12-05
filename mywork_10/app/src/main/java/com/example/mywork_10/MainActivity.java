10a7b7c3a103768ee657a9b7bb294714670b7432
Losia <1064972820@qq.com>
2020-12-05 10:28:20 +0800

    创建ServiceConnection匿名类，并在方法中获取DownloadBinder的实例

--- mywork_10/app/src/main/java/com/example/mywork_10/MainActivity.java
+++ mywork_10/app/src/main/java/com/example/mywork_10/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.mywork_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private DownloadService.DownloadBinder downloadBinder;
 
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (DownloadService.DownloadBinder) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
