51ecc9159bb258f944b1fad04b3bd9627aa8b198
Losia <1064972820@qq.com>
2020-12-05 10:27:17 +0800

    实现事件监听接口并声明成员变量

--- mywork_10/app/src/main/java/com/example/mywork_10/MainActivity.java
+++ mywork_10/app/src/main/java/com/example/mywork_10/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.mywork_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private DownloadService.DownloadBinder downloadBinder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
