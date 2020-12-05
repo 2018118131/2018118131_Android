acbe5c448ffd21925180c7b1cc9053505a01a8b6
Losia <1064972820@qq.com>
2020-12-05 09:39:04 +0800

    声明成员变量

--- mywork_10/app/src/main/java/com/example/mywork_10/DownloadTask.java
+++ mywork_10/app/src/main/java/com/example/mywork_10/DownloadTask.java
@@ -1,4 +1,21 @@
 package com.example.mywork_10;
 
-public class DownloadTask {
+import android.os.AsyncTask;
+
+public class DownloadTask extends AsyncTask<String,Integer,Integer> {
+
+    public static final int TYPE_SUCCESS = 0;
+    public static final int TYPE_FAILED = 1;
+    public static final int TYPE_PAUSED = 2;
+    public static final int TYPE_CANCELED = 3;
+
+    private DownloadListener listener;
+    private boolean isCanceled = false;
+    private boolean isPaused = false;
+    private  int lastProgress;
+
+    @Override
+    protected Integer doInBackground(String... strings) {
+        return null;
+    }
 }
