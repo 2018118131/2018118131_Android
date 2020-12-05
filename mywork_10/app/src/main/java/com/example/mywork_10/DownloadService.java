4f1840246ac99ecbde0b54c188a9c667c7114415
Losia <1064972820@qq.com>
2020-12-05 10:13:03 +0800

    创建DownloadListener匿名类

--- mywork_10/app/src/main/java/com/example/mywork_10/DownloadService.java
+++ mywork_10/app/src/main/java/com/example/mywork_10/DownloadService.java
@@ -1,16 +1,46 @@
 package com.example.mywork_10;
 
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
 
 public class DownloadService extends Service {
 
     private DownloadTask downloadTask;
     private String downloadUrl;
 
+    private DownloadListener listener = new DownloadListener() {
+        @Override
+        public void onProgress(int progress) {
+            getNotificationManager().notify(1, getNotification("Downloading...", progress));
+        }
 
+        @Override
+        public void onSuccess() {
+
+        }
+
+        @Override
+        public void onFailed() {
+
+        }
+
+        @Override
+        public void onPaused() {
+
+        }
+
+        @Override
+        public void onCanceled() {
+
+        }
+    };
 
     public DownloadService() {
     }
@@ -23,6 +53,23 @@ public class DownloadService extends Service {
 
     private NotificationManager getNotificationManager() {
         return (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    private Notification getNotification(String title, int progress) {
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.
+                decodeResource(getResources(),R.mipmap.ic_launcher));
+        builder.setContentIntent(pi);
+        builder.setContentTitle(title);
+        if (progress >= 0){
+            //当 progress 大于或者等于0时才需显示下载进度
+            builder.setContentText(progress + "%");
+            builder.setProgress(100,progress,false);
+        }
+        return builder.build();
     }
 
 }
