f5df8c4d80cae576645f7e319d3b7552e783399b
Losia <1064972820@qq.com>
2020-12-19 23:18:28 +0800

    fragment包-创建MineFragment继承BaseFragment

---
+++ HealthyLife/app/src/main/java/mrkj/healthylife/fragment/MineFragment.java
@@ -0,0 +1,17 @@
+package mrkj.healthylife.fragment;
+
+import android.view.View;
+
+import mrkj.healthylife.base.BaseFragment;
+
+/**
+ * 我的界面
+ *
+ */
+public class MineFragment extends BaseFragment implements View.OnClickListener {
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
