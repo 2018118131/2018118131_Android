f08470259bde428b2197398e3c2ed3415e55d307
Losia <1064972820@qq.com>
2020-12-19 17:29:31 +0800

    base包-BaseActivity类声明变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/base/BaseActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/base/BaseActivity.java
@@ -1,9 +1,14 @@
 package mrkj.healthylife.base;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 public abstract class BaseActivity extends AppCompatActivity{
-
+    private TextView title_center;//标题的中间部分
+    private ImageView title_left,title_right;//标题的左边和右边
+    private RelativeLayout title_relRelativeLayout;
 
 }
