88046011c80fef5d4dfe5be5daf1af2cf5612839
Losia <1064972820@qq.com>
2020-12-19 17:49:15 +0800

    activity包-AboutActivity类重写initValues()方法创建byte数组读取文件数据

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/AboutActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/AboutActivity.java
@@ -1,6 +1,12 @@
 package mrkj.healthylife.activity;
 
+import android.content.res.AssetManager;
 import android.widget.TextView;
+
+import org.apache.http.util.EncodingUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import mrkj.healthylife.base.BaseActivity;
 
@@ -22,7 +28,29 @@ public class AboutActivity extends BaseActivity {
 
     @Override
     protected void initValues() {
-
+        // 获取字符串
+        AssetManager assetManager = getAssets();
+        InputStream is = null;
+        try {
+            is = assetManager.open("about/about.txt");
+            int length = is.available();
+            // 创建byte数组
+            byte[] buffer = new byte[length];
+            // 将文件中的数据读到byte数组中
+            is.read(buffer);
+            aboutTxt = EncodingUtils.getString(buffer, "UTF-8");
+            System.out.println("读取：" + aboutTxt);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
