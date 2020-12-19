1a1adad3ca6a54e753890e035dd1e35297c02517
Losia <1064972820@qq.com>
2020-12-19 13:52:47 +0800

    utils包-GetBMIValuesHelper类声明变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/utils/GetBMIValuesHelper.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/utils/GetBMIValuesHelper.java
@@ -4,5 +4,11 @@ package mrkj.healthylife.utils;
  * 获取BMI
  */
 public class GetBMIValuesHelper {
-
+    public static final int VALUES_NULL = 0;
+    public static final int EXTENUATION = 1;
+    public static final int NORMAL_WEIGHT = 2;
+    public static final int WEIGHT_OVER_WEIGHT = 3;
+    public static final int SEVERLY_OBESE_ONE = 4;
+    public static final int SEVERLY_OBESE_TWO = 5;
+    public static final int SEVERLY_OBESE_THREE = 6;
 }
