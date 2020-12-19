1a98932b8bd657c49654c771e838ca4f64dfb1e7
Losia <1064972820@qq.com>
2020-12-19 23:03:04 +0800

    activity包-WarmUpActivity声明变量并设置标题

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/WarmUpActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/WarmUpActivity.java
@@ -1,13 +1,25 @@
 package mrkj.healthylife.activity;
 
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import mrkj.healthylife.R;
 import mrkj.healthylife.base.BaseActivity;
 
 public class WarmUpActivity extends BaseActivity {
 
-
+    private int what;
+    private ImageView imageView;
+    private TextView type,count,time;
+    private Button start;
     @Override
     protected void setActivityTitle() {
-
+        initTitle();
+        setTitle("推荐热身", this);
+        setMyBackGround(R.color.watm_background_gray);
+        setTitleTextColor(R.color.theme_blue_two);
+        setTitleLeftImage(R.mipmap.mrkj_back_blue);
     }
 
     @Override
