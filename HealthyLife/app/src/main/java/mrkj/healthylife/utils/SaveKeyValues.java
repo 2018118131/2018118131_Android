92fc3c328ac7b8df0ce2a9bb6b4e2fbfb2e79a00
Losia <1064972820@qq.com>
2020-12-19 14:06:01 +0800

    utils包-SaveKeyValues类声明SharedPreferences变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/utils/SaveKeyValues.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/utils/SaveKeyValues.java
@@ -1,9 +1,13 @@
 package mrkj.healthylife.utils;
 
+import android.content.SharedPreferences;
+
 /**
  * Created by Administrator on 2016/5/25.
  */
 public class SaveKeyValues {
 
+    public  static SharedPreferences sharedPreferences;
+    private static SharedPreferences.Editor editor;
 
 }
