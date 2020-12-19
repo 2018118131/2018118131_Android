86b9ce43b7edea0909b5e1d84aaf6c44dafa5ab0
Losia <1064972820@qq.com>
2020-12-19 20:22:19 +0800

    activity包-ShowBPMActivity重写setActivityTitle()方法设置标题“心率测试结果”

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/ShowBPMActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/ShowBPMActivity.java
@@ -1,8 +1,10 @@
 package mrkj.healthylife.activity;
 
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import mrkj.healthylife.R;
 import mrkj.healthylife.base.BaseActivity;
 import mrkj.library.wheelview.circlebar.ColorArcProgressBar;
 
@@ -16,7 +18,12 @@ public class ShowBPMActivity extends BaseActivity {
 
     @Override
     protected void setActivityTitle() {
-
+        initTitle();
+        setMyBackGround(R.color.watm_background_gray);
+        share = setTitle("心率测试结果", this, R.mipmap.mrkj_share_blue);
+        setTitleLeftImage(R.mipmap.mrkj_back_blue);
+        setTitleTextColor(R.color.theme_blue_two);
+        share.setVisibility(View.INVISIBLE);
     }
 
     @Override
