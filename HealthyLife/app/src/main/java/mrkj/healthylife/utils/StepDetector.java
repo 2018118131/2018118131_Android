ee6d888fb5c5a570942da1d12585294586db1743
Losia <1064972820@qq.com>
2020-12-19 14:17:49 +0800

    utils包-StepDetector类定义相关变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/utils/StepDetector.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/utils/StepDetector.java
@@ -1,5 +1,6 @@
 package mrkj.healthylife.utils;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -12,6 +13,16 @@ import android.hardware.SensorEventListener;
  */
 public class StepDetector implements SensorEventListener {
 
+	public static int CURRENT_SETP = 0;
+	public int walk = 0;
+	public static float SENSITIVITY = 8; // SENSITIVITY灵敏度
+
+	private float mLastValues[] = new float[3 * 2];
+	private float mScale[] = new float[2];
+	private float mYOffset;//位移
+	private static long mEnd = 0;//运动相隔时间
+	private static long mStart = 0;//运动开始时间
+	private Context context;
 
 	@Override
 	public void onSensorChanged(SensorEvent sensorEvent) {
