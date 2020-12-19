89da62878b540632b15af80ca47d819ec37771da
Losia <1064972820@qq.com>
2020-12-19 13:34:02 +0800

    utils包-constant类：定义每个Fragment的TAG值

--- HealthyLife/app/src/main/java/mrkj/healthylife/utils/Constant.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/utils/Constant.java
@@ -9,5 +9,10 @@ public class Constant {
     public static final int TURN_MAIN = 1;//加载运动界面
     public static final int MAKE_PLAN = 2;//加载发现界面
 
+    //每个Fragment的Tag值
+    public static final String SPORT_TAG = "sport";//运动TAG
+    public static final String FIND_TAG = "find";//发现TAG
+    public static final String HEART_TAG = "heart";//心率TAG
+    public static final String MINE_TAG = "mine";//我的TAG
 
 }
