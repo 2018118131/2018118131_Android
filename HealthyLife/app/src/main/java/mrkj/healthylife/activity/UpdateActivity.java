0d4f3d60dab2c5eec42e85a4f1055a9be6610ea7
Losia <1064972820@qq.com>
2020-12-19 22:45:56 +0800

    activity包-创建UpdateActivity

---
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/UpdateActivity.java
@@ -0,0 +1,19 @@
+package mrkj.healthylife.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TimePicker;
+
+public class UpdateActivity extends AppCompatActivity implements View.OnClickListener,TimePicker.OnTimeChangedListener{
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+
+    }
+}
