815b0043930994498cfc9bad99344d0dbd89f399
Losia <1064972820@qq.com>
2020-12-19 23:04:06 +0800

    activity包-WarmUpActivity重写方法初始化变量，控件，布局文件，监听事件以及相关功能

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/WarmUpActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/WarmUpActivity.java
@@ -1,11 +1,16 @@
 package mrkj.healthylife.activity;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import mrkj.healthylife.R;
+import mrkj.healthylife.application.DemoApplication;
 import mrkj.healthylife.base.BaseActivity;
+import mrkj.healthylife.fragment.FindFragment;
 
 public class WarmUpActivity extends BaseActivity {
 
@@ -24,26 +29,41 @@ public class WarmUpActivity extends BaseActivity {
 
     @Override
     protected void getLayoutToView() {
-
+        setContentView(R.layout.activity_warm_up);
     }
 
     @Override
     protected void initValues() {
-
+        what = getIntent().getIntExtra("random",0);
+        Log.e("随机数", "【" + what + "】");
     }
 
     @Override
     protected void initViews() {
-
+        imageView = (ImageView) findViewById(R.id.image_show);
+        type = (TextView) findViewById(R.id.xiangmu);
+        count = (TextView) findViewById(R.id.cishu1);
+        time = (TextView) findViewById(R.id.add_plan);
+        start = (Button) findViewById(R.id.start_warm);
     }
 
     @Override
     protected void setViewsListener() {
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WarmUpActivity.this,PlayActivity.class).putExtra("play_type",what));
+                finish();
+            }
+        });
 
     }
 
     @Override
     protected void setViewsFunction() {
-
+        time.setText("12秒");
+        count.setText(FindFragment.cishu);
+        type.setText(DemoApplication.shuoming[what]);
+        imageView.setImageBitmap(DemoApplication.bitmaps[what]);
     }
 }
