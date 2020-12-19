962b77346d6fa002b1b8de387b577bd6596e30a0
Losia <1064972820@qq.com>
2020-12-19 20:40:19 +0800

    activity包-SportMessageActivity声明变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/SportMessageActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/SportMessageActivity.java
@@ -1,10 +1,37 @@
 package mrkj.healthylife.activity;
 
+import android.database.Cursor;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
 
 import mrkj.healthylife.base.BaseActivity;
+import mrkj.healthylife.db.DatasDao;
 
 public class SportMessageActivity extends BaseActivity implements View.OnClickListener{
+
+    private TextView finish_plans;
+    private TextView sport_days;
+    private TextView sport_hot;
+    private TextView keepfit_scores;
+    private int plans;
+    private String hot_str;
+    private int day_values;
+    private int scores;
+    private DatasDao datasDao;
+    private Button all_btn,day_btn;
+    private LinearLayout all_lin;
+    private RelativeLayout day_lin;
+    private Cursor cursor;
+    private ListView dataList;
+    private int counts;
+    private List<Map<String , Object>> list;
 
 
     @Override
