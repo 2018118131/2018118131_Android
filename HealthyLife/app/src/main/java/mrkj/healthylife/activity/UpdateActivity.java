43c6d525523c0fbb58aae706762d80ab89c85fd5
Losia <1064972820@qq.com>
2020-12-19 22:52:54 +0800

    activity包-UpdateActivity声明变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/UpdateActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/UpdateActivity.java
@@ -2,9 +2,23 @@ package mrkj.healthylife.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
+import mrkj.healthylife.db.DatasDao;
+
 public class UpdateActivity extends AppCompatActivity implements View.OnClickListener,TimePicker.OnTimeChangedListener{
+
+
+    private Button change_stop_date,dimind_change_date;
+    private TextView back_to_front;
+    private TimePicker change_time;
+    private int hour,minute;
+    private int change_year,change_month,change_day;
+    private int index;
+    private int id;
+    private DatasDao datasDao;
 
 
     @Override
