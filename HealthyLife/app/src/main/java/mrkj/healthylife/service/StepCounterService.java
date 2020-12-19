341032d3cf053edb0dad09f6a26e787316d7fd86
Losia <1064972820@qq.com>
2020-12-19 21:46:54 +0800

    service包-StepCounterService声明变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/service/StepCounterService.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/service/StepCounterService.java
@@ -1,10 +1,18 @@
 package mrkj.healthylife.service;
 
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.hardware.SensorManager;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.support.annotation.Nullable;
+
+import java.util.Calendar;
+
+import mrkj.healthylife.utils.StepDetector;
 
 /**
  * 计步服务
@@ -12,6 +20,20 @@ import android.support.annotation.Nullable;
  * @author Administrator
  */
 public class StepCounterService extends Service {
+
+    public static final String alarmSaveService = "mrkj.healthylife.SETALARM";
+    private static final String TAG = "StepCounterService";
+    public static Boolean FLAG = false;// 服务运行标志
+
+    private SensorManager mSensorManager;// 传感器服务
+    public StepDetector detector;// 传感器监听对象
+
+    private PowerManager mPowerManager;// 电源管理服务
+    private PowerManager.WakeLock mWakeLock;// 屏幕灯
+    private AlarmManager alarmManager;//闹钟管理器
+    private PendingIntent pendingIntent;//延迟意图
+    private Calendar calendar;//日期
+    private Intent intent;//意图
 
 
     @Nullable
