caa71ab45562afe56dfd01cdc26a9c6743cc3dc0
Losia <1064972820@qq.com>
2020-12-19 23:52:03 +0800

    activity包-FunctionActivity重写方法初始化控件，布局，设置监听，功能

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/FunctionActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/FunctionActivity.java
@@ -2,6 +2,7 @@ package mrkj.healthylife.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -14,6 +15,7 @@ import mrkj.healthylife.fragment.HeartFragment;
 import mrkj.healthylife.fragment.MineFragment;
 import mrkj.healthylife.fragment.SportFragment;
 import mrkj.healthylife.utils.Constant;
+import mrkj.healthylife.utils.SaveKeyValues;
 
 /**
  * 功能界面
@@ -72,34 +74,77 @@ public class FunctionActivity extends BaseActivity implements RadioGroup.OnCheck
         transaction.commit();
     }
 
+    /**
+     * 设置标题
+     */
     @Override
     protected void setActivityTitle() {
 
     }
 
+    /**
+     * 初始化界面
+     */
     @Override
     protected void getLayoutToView() {
-
+        setContentView(R.layout.activity_function);
     }
 
+    /**
+     * 初始化相关变量
+     */
     @Override
     protected void initValues() {
 
+        //如果这个值等于1就加载运动界面，等于2就加载发现界面
+        load_values = SaveKeyValues.getIntValues("launch_which_fragment",0);
+        Log.e("加载判断值", load_values + "");
+        //实例化相关碎片
+        sportFragment = new SportFragment();
+        findFragment = new FindFragment();
+        heartFragment = new HeartFragment();
+        mineFragment = new MineFragment();
+        //初始化界面
+        if (load_values == Constant.TURN_MAIN){
+            Bundle bundle = new Bundle();
+            bundle.putBoolean("is_launch",true);
+            sportFragment.setArguments(bundle);
+            getSupportFragmentManager().beginTransaction().add(R.id.frag_home,sportFragment,Constant.SPORT_TAG).commit();
+        }else {
+            getSupportFragmentManager().beginTransaction().add(R.id.frag_home,findFragment,Constant.FIND_TAG).commit();
+        }
     }
 
+    /**
+     * 初始化控件
+     */
     @Override
     protected void initViews() {
-
+        radioGroup = (RadioGroup) findViewById(R.id.ui_btn_group);
+        sport_btn = (RadioButton) findViewById(R.id.sport_btn);
+        find_btn = (RadioButton) findViewById(R.id.find_btn);
+        heart_btn = (RadioButton) findViewById(R.id.heart_btn);
+        mine_btn = (RadioButton) findViewById(R.id.mine_btn);
     }
 
+    /**
+     * 设置监听
+     */
     @Override
     protected void setViewsListener() {
-
+        radioGroup.setOnCheckedChangeListener(this);
     }
 
+    /**
+     * 设置功能
+     */
     @Override
     protected void setViewsFunction() {
-
+        if (load_values == Constant.TURN_MAIN){
+            sport_btn.setChecked(true);
+        }else {
+            find_btn.setChecked(true);
+        }
     }
 
     /**
