2065e83bddf862faedbd2b8d4bb5ffa3cd1c7ff7
Losia <1064972820@qq.com>
2020-12-19 17:47:52 +0800

    activity包-AboutActivity类继承BaseActivity并声明标题栏控件

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/AboutActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/AboutActivity.java
@@ -1,7 +1,42 @@
 package mrkj.healthylife.activity;
 
+import android.widget.TextView;
+
 import mrkj.healthylife.base.BaseActivity;
 
 public class AboutActivity extends BaseActivity {
 
+    // 声明标题栏控件
+    private TextView showTxt;
+    private String aboutTxt;
+
+    @Override
+    protected void setActivityTitle() {
+
+    }
+
+    @Override
+    protected void getLayoutToView() {
+
+    }
+
+    @Override
+    protected void initValues() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void setViewsListener() {
+
+    }
+
+    @Override
+    protected void setViewsFunction() {
+
+    }
 }
