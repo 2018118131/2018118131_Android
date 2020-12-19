e349b44e91eadf82f68409b1d3714b9130a713d7
Losia <1064972820@qq.com>
2020-12-19 19:51:45 +0800

    activity包-PlayActivity重写getLayoutToView()方法初始化布局文件

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/PlayActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/PlayActivity.java
@@ -93,7 +93,7 @@ public class PlayActivity extends BaseActivity implements View.OnClickListener{
 
     @Override
     protected void getLayoutToView() {
-
+        setContentView(R.layout.activity_play);
     }
 
     @Override
