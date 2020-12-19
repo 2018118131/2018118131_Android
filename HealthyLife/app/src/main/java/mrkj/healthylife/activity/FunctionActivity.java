8d39b095ce19c4137d9f0b871b29ace29fe35658
Losia <1064972820@qq.com>
2020-12-19 19:10:38 +0800

    activity包创建FunctionActivity

---
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/FunctionActivity.java
@@ -0,0 +1,47 @@
+package mrkj.healthylife.activity;
+
+import android.widget.RadioGroup;
+
+import mrkj.healthylife.base.BaseActivity;
+
+/**
+ * 功能界面
+ */
+public class FunctionActivity extends BaseActivity implements RadioGroup.OnCheckedChangeListener{
+
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+    }
+
+    @Override
+    protected void setActivityTitle() {
+
+    }
+
+    @Override
+    protected void getLayoutToView() {
+
+    }
+
+    @Override
+    protected void initValues() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void setViewsListener() {
+
+    }
+
+    @Override
+    protected void setViewsFunction() {
+
+    }
+}
