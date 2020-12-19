f0ed50c4a483107796e2386073d322336e262ddb
Losia <1064972820@qq.com>
2020-12-19 21:57:41 +0800

    service包-BootBroadcastReceiver定义变量和构造方法

--- HealthyLife/app/src/main/java/mrkj/healthylife/service/RecordedSaveService.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/service/RecordedSaveService.java
@@ -5,10 +5,17 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import mrkj.healthylife.db.DatasDao;
+
 /**
  * 记录保存服务(此处用于记录值)
  */
 public class RecordedSaveService extends Service {
+
+    public static final String cancelSaveService = "mrkj.healthylife.RECORDED";
+    private DatasDao datasDao;
+    public RecordedSaveService() {
+    }
 
     @Nullable
     @Override
