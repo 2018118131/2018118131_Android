48e8a3dceec0f3cc16416580e14b45e3b7035636
Losia <1064972820@qq.com>
2020-12-19 22:20:50 +0800

    fragment包-创建SportFragment

---
+++ HealthyLife/app/src/main/java/mrkj/healthylife/fragment/SportFragment.java
@@ -0,0 +1,14 @@
+package mrkj.healthylife.fragment;
+
+import mrkj.healthylife.base.BaseFragment;
+
+/**
+ * 运动
+ * 功能介绍显示所在城市的空气质量和温度
+ * 展示走步的进度
+ * 展示一走的里程和消耗的热量
+ * 跳转界面的功能按钮
+ */
+public class SportFragment extends BaseFragment {//此处直接继承Fragment即可
+
+}
