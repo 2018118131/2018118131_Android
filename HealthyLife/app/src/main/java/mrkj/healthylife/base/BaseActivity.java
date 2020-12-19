1417a9108d9e19bca322b2a06dd7db3fe60590ac
Losia <1064972820@qq.com>
2020-12-19 17:30:24 +0800

    base包-BaseActivity定义initTitle()方法初始化控件变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/base/BaseActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/base/BaseActivity.java
@@ -1,14 +1,29 @@
 package mrkj.healthylife.base;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import mrkj.healthylife.R;
 
 
 public abstract class BaseActivity extends AppCompatActivity{
     private TextView title_center;//标题的中间部分
     private ImageView title_left,title_right;//标题的左边和右边
     private RelativeLayout title_relRelativeLayout;
+
+    /**
+     * 初始化标题
+     */
+    public void initTitle(){
+        title_center = (TextView) findViewById(R.id.titles);
+        title_left = (ImageView) findViewById(R.id.left_btn);
+        title_right = (ImageView) findViewById(R.id.right_btn);
+        title_left.setVisibility(View.INVISIBLE);
+        title_right.setVisibility(View.INVISIBLE);
+        title_relRelativeLayout = (RelativeLayout) findViewById(R.id.title_back);
+    }
 
 }
