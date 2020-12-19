6dd83dbaff6b71883ab7e75ddfc3bb247ab83a24
Losia <1064972820@qq.com>
2020-12-19 21:52:37 +0800

    service包-创建BootBroadcastReceiver

---
+++ HealthyLife/app/src/main/java/mrkj/healthylife/service/BootBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package mrkj.healthylife.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * 用于开机自启动
+ *
+// */
+public class BootBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
