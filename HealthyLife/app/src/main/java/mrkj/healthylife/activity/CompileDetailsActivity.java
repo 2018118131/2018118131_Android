ac281bb1568f249248c5a1a5a441727e4bc1a1ad
Losia <1064972820@qq.com>
2020-12-19 18:09:07 +0800

    activity包-CompileDetailsActivity声明更换头像的变量：显示头像，更换头像，头像的路径，图片路径

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/CompileDetailsActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/CompileDetailsActivity.java
@@ -1,8 +1,12 @@
 package mrkj.healthylife.activity;
 
 import android.view.View;
+import android.widget.TextView;
+
+import java.io.File;
 
 import mrkj.healthylife.base.BaseActivity;
+import mrkj.library.wheelview.circleimageview.CircleImageView;
 
 /**
  * 更改用户信息
@@ -14,6 +18,12 @@ public class CompileDetailsActivity extends BaseActivity implements View.OnClick
     private static final int PHOTO_REQUEST_GALLERY2 = 4;// 从相册中选择
     private static final int PHOTO_REQUEST_CUT = 3;// 结果
     private static final String PHOTO_FILE_NAME = "temp_photo.jpg";// 图片名称
+
+    //1、更换头像
+    private CircleImageView head_image;//显示头像
+    private TextView change_image;//更换头像
+    private String path;//头像的路径
+    private File tempFile;//图片路径
 
     @Override
     public void onClick(View view) {
