c912798d0aeefdaed5c0e269a12066aa8a55c7e3
Losia <1064972820@qq.com>
2020-12-19 13:38:51 +0800

    utils包-DateUtils类定义Map()方法获取日期

--- HealthyLife/app/src/main/java/mrkj/healthylife/utils/DateUtils.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/utils/DateUtils.java
@@ -1,9 +1,32 @@
 package mrkj.healthylife.utils;
 
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 获取日期
  */
 public class DateUtils {
-
+    /**
+     * 获取日期
+     * @return
+     */
+    public static Map<String,Object> getDate(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        Map<String,Object> map = new HashMap<>();
+        map.put("year",year);
+        map.put("month",month);
+        map.put("day",day);
+        map.put("hour",hour);
+        map.put("minute",minute);
+        map.put("date",year+"-"+month+"-"+day);
+        return map;
+    }
 
 }
