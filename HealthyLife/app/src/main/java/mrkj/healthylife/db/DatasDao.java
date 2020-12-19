426a51404ce55ac3157f9dd304205f23d82ef57e
Losia <1064972820@qq.com>
2020-12-19 14:46:21 +0800

    db包-DatasDao类声明变量并定义构造方法实例化对象

--- HealthyLife/app/src/main/java/mrkj/healthylife/db/DatasDao.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/db/DatasDao.java
@@ -1,8 +1,19 @@
 package mrkj.healthylife.db;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  *
  */
 public class DatasDao {
+    private DatasDB myDB;
+    private SQLiteDatabase db;
 
+    public DatasDao(Context context) {
+        // 实例化dbhelper类
+        myDB = new DatasDB(context);
+        // 得到一个数据库对象
+        db = myDB.getWritableDatabase();
+    }
 }
