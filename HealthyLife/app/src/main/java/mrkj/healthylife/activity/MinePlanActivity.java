f20c6b71fc3a5550b4dc1c0749a6ece874e6142b
Losia <1064972820@qq.com>
2020-12-19 22:35:02 +0800

    activity包-MinePlanActivity声明变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/MinePlanActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/MinePlanActivity.java
@@ -1,9 +1,21 @@
 package mrkj.healthylife.activity;
 
+import android.database.Cursor;
+import android.widget.ListView;
+
+import java.util.List;
+import java.util.Map;
+
 import mrkj.healthylife.base.BaseActivity;
+import mrkj.healthylife.db.DatasDao;
 
 public class MinePlanActivity extends BaseActivity {
 
+    private List<Map<String,Object>> plan_List;//储存计划数据
+    private ListView listView;//列表
+    private DatasDao datasDao;//数据库操作诶
+    private Cursor cursor;//游标
+    private MyAdapter myAdapter;//适配器
 
     @Override
     protected void setActivityTitle() {
