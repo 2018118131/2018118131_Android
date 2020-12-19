eb1557ba3b69a972d795596b4b7600fcadd4bc49
Losia <1064972820@qq.com>
2020-12-19 17:54:31 +0800

    activity包-创建CompileDetailsActivity更改用户信息

---
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/CompileDetailsActivity.java
@@ -0,0 +1,47 @@
+package mrkj.healthylife.activity;
+
+import android.view.View;
+
+import mrkj.healthylife.base.BaseActivity;
+
+/**
+ * 更改用户信息
+ */
+public class CompileDetailsActivity extends BaseActivity implements View.OnClickListener{
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    protected void setActivityTitle() {
+
+    }
+
+    @Override
+    protected void getLayoutToView() {
+
+    }
+
+    @Override
+    protected void initValues() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void setViewsListener() {
+
+    }
+
+    @Override
+    protected void setViewsFunction() {
+
+    }
+}
