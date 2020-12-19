d87b646bfbc31e33572e38e54dcdc8976a477c17
Losia <1064972820@qq.com>
2020-12-19 22:38:33 +0800

    activity包-MinePanActivity重写  initValues()初始化数据

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/MinePlanActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/MinePlanActivity.java
@@ -3,6 +3,8 @@ package mrkj.healthylife.activity;
 import android.database.Cursor;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -36,9 +38,39 @@ public class MinePlanActivity extends BaseActivity {
         setContentView(R.layout.activity_mine_plan);
     }
 
+    /**
+     * 初始化数据
+     */
     @Override
     protected void initValues() {
-
+        plan_List = new ArrayList<>();
+        datasDao = new DatasDao(this);
+        cursor = datasDao.selectAll("plans");//获取游标用来查询
+        //遍历所有数据
+        while (cursor.moveToNext()){
+            Map<String,Object> map = new HashMap<>();
+            int id = cursor.getInt(cursor.getColumnIndex("_id"));
+            int type = cursor.getInt(cursor.getColumnIndex("sport_type"));
+            int s_year = cursor.getInt(cursor.getColumnIndex("start_year"));
+            int s_month = cursor.getInt(cursor.getColumnIndex("start_month"));
+            int s_day = cursor.getInt(cursor.getColumnIndex("start_day"));
+            int t_year = cursor.getInt(cursor.getColumnIndex("stop_year"));
+            int t_month = cursor.getInt(cursor.getColumnIndex("stop_month"));
+            int t_day = cursor.getInt(cursor.getColumnIndex("stop_day"));
+            String h_time = cursor.getString(cursor.getColumnIndex("hint_str"));
+            if (s_year == t_year && s_month == t_month & s_day == t_day){
+                map.put("date",s_year+"-"+s_month+"-"+s_day);
+            }else {
+                map.put("date",s_year+"-"+s_month+"-"+s_day+"~"+t_year+"-"+t_month+"-"+t_day);
+            }
+            map.put("id",id);
+            map.put("type",type);
+            map.put("time",h_time);
+            //向集合中添加数据
+            plan_List.add(map);
+        }
+        //关闭游标
+        cursor.close();
     }
 
     @Override
