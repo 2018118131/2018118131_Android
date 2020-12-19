3ec14fc23491bc480fd5421644683f412e1d262c
Losia <1064972820@qq.com>
2020-12-19 21:59:31 +0800

    service包-BootBroadcastReceiver定义 onCreate()用于步数方面计算

--- HealthyLife/app/src/main/java/mrkj/healthylife/service/RecordedSaveService.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/service/RecordedSaveService.java
@@ -1,11 +1,18 @@
 package mrkj.healthylife.service;
 
 import android.app.Service;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.Map;
 
 import mrkj.healthylife.db.DatasDao;
+import mrkj.healthylife.utils.DateUtils;
+import mrkj.healthylife.utils.SaveKeyValues;
+import mrkj.healthylife.utils.StepDetector;
 
 /**
  * 记录保存服务(此处用于记录值)
@@ -22,4 +29,51 @@ public class RecordedSaveService extends Service {
     public IBinder onBind(Intent intent) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e("测试服务","启动了！");
+        int custom_step_length = SaveKeyValues.getIntValues("length",70);
+        int custom_weight = SaveKeyValues.getIntValues("weight", 50);
+        //创建数据库工具类
+        datasDao = new DatasDao(this);
+        boolean result = isActivityRunning(this);
+        int step;
+        //判断如何存储数值
+        if (result){//获取步数直接获取
+            step = StepDetector.CURRENT_SETP;
+        }else {//获取xml存值加啥记录步数
+            step = StepDetector.CURRENT_SETP + SaveKeyValues.getIntValues("sport_steps" ,0);
+        }
+        //通过步数计算消耗的热量和行走的路程
+        double distance_values = step * custom_step_length * 0.01 *0.001;//km
+        String distance_Str = formatDouble(distance_values);
+        double heat_values = custom_weight * distance_values * 1.036;//cls
+        String heat_Str = formatDouble(heat_values);
+        //获取日期
+        Map<String,Object> map = DateUtils.getDate();
+        int year = (int) map.get("year");
+        int month = (int) map.get("month");
+        int day = (int) map.get("day");
+        String date = (String) map.get("date");
+        //存入数据
+        ContentValues values = new ContentValues();
+        values.put("date",date);
+        values.put("year",year);
+        values.put("month",month);
+        values.put("day",day);
+        values.put("steps",step);
+        values.put("hot",heat_Str);
+        values.put("length", distance_Str);
+        long reBack = datasDao.insertValue("step",values);
+        if (reBack > 0){
+            SaveKeyValues.putIntValues("sport_steps", 0 );
+            StepDetector.CURRENT_SETP = 0;
+            Intent bro = new Intent(this, FunctionBroadcastReceiver.class);
+            bro.setAction(cancelSaveService);
+            sendBroadcast(bro);
+        }
+    }
+
 }
