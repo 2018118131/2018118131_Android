969727864296afae1e738f8d635ccfbdebf5bb21
Losia <1064972820@qq.com>
2020-12-19 19:13:47 +0800

    activity包-LaunchActivity添加变量判断是否为第一次启动

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/LaunchActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/LaunchActivity.java
@@ -1,10 +1,27 @@
 package mrkj.healthylife.activity;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * 启动页
  */
 public class LaunchActivity extends AppCompatActivity {
-
+    private boolean isFirst;//是否为第一次启动
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            if (msg.what == 1) {
+                if (isFirst){
+                    startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+                }else {
+                    startActivity(new Intent(LaunchActivity.this, FunctionActivity.class));
+                }
+                finish();
+            }
+            return false;
+        }
+    });
 }
