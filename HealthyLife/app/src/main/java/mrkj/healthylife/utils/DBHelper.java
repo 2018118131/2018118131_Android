c9fa140c5432371f6d6c35cb1b5861aeb6b858d5
Losia <1064972820@qq.com>
2020-12-19 13:44:01 +0800

    utils包-DBHelper类声明数据库变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/utils/DBHelper.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/utils/DBHelper.java
@@ -1,8 +1,15 @@
 package mrkj.healthylife.utils;
 
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * 数据库
  */
 public class DBHelper {
+
+    private SQLiteDatabase db;
+    public DBHelper(){
+        db = SQLiteDatabase.openOrCreateDatabase(BringData.DATA_PATH+BringData.DATA_NAME,null);
+    }
 
 }
