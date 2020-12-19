8c9df7e8586fb8bf45e81d1264d87f7c4f9d2266
Losia <1064972820@qq.com>
2020-12-19 23:19:56 +0800

    fragment包-MineFragment定义onCreateView()方法初始化控件显示视图

--- HealthyLife/app/src/main/java/mrkj/healthylife/fragment/MineFragment.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/fragment/MineFragment.java
@@ -1,15 +1,21 @@
 package mrkj.healthylife.fragment;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import lecho.lib.hellocharts.model.LineChartData;
 import lecho.lib.hellocharts.view.LineChartView;
+import mrkj.healthylife.R;
 import mrkj.healthylife.base.BaseFragment;
 import mrkj.healthylife.db.DatasDao;
+import mrkj.healthylife.utils.SaveKeyValues;
 import mrkj.library.wheelview.circleimageview.CircleImageView;
 
 /**
@@ -42,6 +48,40 @@ public class MineFragment extends BaseFragment implements View.OnClickListener {
     public void onAttach(Context context) {
         super.onAttach(context);
         this.context = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_mine, null);
+        //1、第一部分显示头像、昵称
+        head_image = (CircleImageView) view.findViewById(R.id.head_pic);
+        custom_name = (TextView) view.findViewById(R.id.show_name);
+        change_values = (ImageButton) view.findViewById(R.id.change_person_values);
+        //点击跳转到编辑个人信息界面
+        change_values.setOnClickListener(this);
+        //2、第二部分显示当日的步数和历史统计图
+        show_steps = (TextView) view.findViewById(R.id.show_steps);
+        lineChartView = (LineChartView) view.findViewById(R.id.step_chart);
+        if (isAdded()) {
+            datasDao = new DatasDao(getContext());
+        }
+        //显示信息
+        showMessage();
+        //3.初始化其余相关控件并添加点击事件的监听
+        food = (TextView) view.findViewById(R.id.food_hot);
+        food.setOnClickListener(this);
+        want = (TextView) view.findViewById(R.id.want);
+        want.setText("在" + SaveKeyValues.getStringValues("plan_stop_date","2016年6月16日")+"体重达到【"+SaveKeyValues.getIntValues("weight",50)+"】公斤");
+        about = (TextView) view.findViewById(R.id.about_btn);
+        about.setOnClickListener(this);
+        sport_message = (TextView) view.findViewById(R.id.sport_btn);
+        sport_message.setOnClickListener(this);
+        steps = (EditText) view.findViewById(R.id.change_step);
+        steps.setText(SaveKeyValues.getIntValues("step_plan" , 6000) + "");
+        plan_btn = (TextView) view.findViewById(R.id.plan_btn);
+        plan_btn.setOnClickListener(this);
+        return view;
     }
 
     @Override
