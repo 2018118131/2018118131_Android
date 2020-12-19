ae586487d944c9658dacbe112ab9020002c76abb
Losia <1064972820@qq.com>
2020-12-19 14:06:53 +0800

    utils包-SaveKeyValues类定义createSharePreferences()方法初始化SharedPreferences

--- HealthyLife/app/src/main/java/mrkj/healthylife/utils/SaveKeyValues.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/utils/SaveKeyValues.java
@@ -1,6 +1,8 @@
 package mrkj.healthylife.utils;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 /**
  * Created by Administrator on 2016/5/25.
@@ -9,5 +11,16 @@ public class SaveKeyValues {
 
     public  static SharedPreferences sharedPreferences;
     private static SharedPreferences.Editor editor;
+
+    /**
+     * 初始化SharedPreferences
+     * @param context
+     */
+    public static void createSharePreferences(Context context){
+        String appName = context.getPackageName();
+        Log.e("储存的文件名",appName);
+        sharedPreferences = context.getSharedPreferences(appName, Context.MODE_WORLD_WRITEABLE);
+        editor = sharedPreferences.edit();
+    }
 
 }
