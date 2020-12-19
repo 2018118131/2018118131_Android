64de0f2d20b59dbef0f90189f330dab373279863
Losia <1064972820@qq.com>
2020-12-19 18:07:24 +0800

    activity包-CompileDetailsActivity继承BaseActivity并声明Static变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/CompileDetailsActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/CompileDetailsActivity.java
@@ -9,6 +9,11 @@ import mrkj.healthylife.base.BaseActivity;
  */
 public class CompileDetailsActivity extends BaseActivity implements View.OnClickListener{
 
+    private static final int PHOTO_REQUEST_CAMERA = 1;// 拍照
+    private static final int PHOTO_REQUEST_GALLERY = 2;// 从相册中选择
+    private static final int PHOTO_REQUEST_GALLERY2 = 4;// 从相册中选择
+    private static final int PHOTO_REQUEST_CUT = 3;// 结果
+    private static final String PHOTO_FILE_NAME = "temp_photo.jpg";// 图片名称
 
     @Override
     public void onClick(View view) {
