f8e4bed855d2847283acf7aa6e8f818bcda8b47f
Losia <1064972820@qq.com>
2020-12-19 14:26:14 +0800

    utils包-HttpUtils类声明变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/utils/HttpUtils.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/utils/HttpUtils.java
@@ -7,6 +7,10 @@ package mrkj.healthylife.utils;
  *
  */
 public class HttpUtils {
-
-
+    private static String userAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
+    private static final int DEF_CONN_TIMEOUT = 30000;
+    private static final int DEF_READ_TIMEOUT = 30000;
+    private static final String DEF_CHATSET = "UTF-8";
+    public static final String HUMIDITY = "%rh";// 湿度单位
+    public static final String TEMPERATURE = "°C";// 温度单位
 }
