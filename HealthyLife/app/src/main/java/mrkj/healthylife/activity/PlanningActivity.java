49c7b06ebf4a7edd132e3683598ec3473ba229ac
Losia <1064972820@qq.com>
2020-12-19 19:18:20 +0800

    activity包-创建PlanningActivity并继承BaseActivity用于设置“制定计划”

---
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/PlanningActivity.java
@@ -0,0 +1,52 @@
+package mrkj.healthylife.activity;
+
+import android.view.View;
+
+import mrkj.healthylife.base.BaseActivity;
+
+/**
+ * 制定计划-->设置相关信息
+ */
+public class PlanningActivity extends BaseActivity implements View.OnFocusChangeListener,View.OnClickListener{
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean b) {
+
+    }
+
+    @Override
+    protected void setActivityTitle() {
+
+    }
+
+    @Override
+    protected void getLayoutToView() {
+
+    }
+
+    @Override
+    protected void initValues() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void setViewsListener() {
+
+    }
+
+    @Override
+    protected void setViewsFunction() {
+
+    }
+}
