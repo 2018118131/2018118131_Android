72f7d00d1a5fa00f9d6455c623e4f283ad66737c
Losia <1064972820@qq.com>
2020-12-19 18:33:02 +0800

    activity包-FoodHotListActivity声明变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/FoodHotListActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/FoodHotListActivity.java
@@ -1,8 +1,21 @@
 package mrkj.healthylife.activity;
 
+import android.graphics.Bitmap;
+import android.widget.ExpandableListView;
+
+import java.util.List;
+
 import mrkj.healthylife.base.BaseActivity;
+import mrkj.healthylife.entity.FoodType;
 
 public class FoodHotListActivity extends BaseActivity {
+
+    private int sign= - 1 ; //控制列表的展开
+    private String[] food_type_array;//食物类型数组
+    private List<FoodType> food_list;//数据集合
+    private ExpandableListView data_list;//折叠listview
+    private Bitmap[] bitmaps;//图片资源
+    private int[] ids;//图片资源ID数组
 
     @Override
     protected void setActivityTitle() {
