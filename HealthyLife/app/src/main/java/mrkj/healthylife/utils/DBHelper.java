318f91327954a9cf1896eb5639b4d3828d9a9ed5
Losia <1064972820@qq.com>
2020-12-19 13:44:45 +0800

    utils包-DBHelper类定义select()方法查询数据

--- HealthyLife/app/src/main/java/mrkj/healthylife/utils/DBHelper.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/utils/DBHelper.java
@@ -1,5 +1,6 @@
 package mrkj.healthylife.utils;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
@@ -10,6 +11,25 @@ public class DBHelper {
     private SQLiteDatabase db;
     public DBHelper(){
         db = SQLiteDatabase.openOrCreateDatabase(BringData.DATA_PATH+BringData.DATA_NAME,null);
+    }
+
+    /**
+     * 查询数据库
+     * @param table
+     * @param columns
+     * @param selection
+     * @param selectionArgs
+     * @param groupBy
+     * @param having
+     * @param orderBy
+     * @return
+     */
+    public Cursor select (String table, String[] columns,
+                          String selection, String[] selectionArgs, String groupBy,
+                          String having, String orderBy){
+        return  db.query(table,columns,selection,selectionArgs,groupBy,having,orderBy);
+
+
     }
 
 }
