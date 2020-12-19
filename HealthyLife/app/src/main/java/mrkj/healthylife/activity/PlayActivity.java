c319e006aa4f3171bdbca7b856a36df444f356a0
Losia <1064972820@qq.com>
2020-12-19 19:37:58 +0800

    activity包-创建PlayActivity并继承BaseActivity处理播放热身动画

---
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/PlayActivity.java
@@ -0,0 +1,44 @@
+package mrkj.healthylife.activity;
+
+import android.view.View;
+
+import mrkj.healthylife.base.BaseActivity;
+
+public class PlayActivity extends BaseActivity implements View.OnClickListener{
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    protected void setActivityTitle() {
+
+    }
+
+    @Override
+    protected void getLayoutToView() {
+
+    }
+
+    @Override
+    protected void initValues() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void setViewsListener() {
+
+    }
+
+    @Override
+    protected void setViewsFunction() {
+
+    }
+}
