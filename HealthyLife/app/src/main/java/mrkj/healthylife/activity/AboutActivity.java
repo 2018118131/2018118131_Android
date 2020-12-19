86a87aec55f5766a7cf50d8bc76d06e2406cf1fb
Losia <1064972820@qq.com>
2020-12-19 17:51:43 +0800

    activity包-AboutActivity类重写setActivityTitle()方法初始化方法和变量

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/AboutActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/AboutActivity.java
@@ -19,7 +19,11 @@ public class AboutActivity extends BaseActivity {
 
     @Override
     protected void setActivityTitle() {
-
+        initTitle();
+        setTitle("关于我们", this);
+        setMyBackGround(R.color.watm_background_gray);
+        setTitleTextColor(R.color.theme_blue_two);
+        setTitleLeftImage(R.mipmap.mrkj_back_blue);
     }
 
     @Override
