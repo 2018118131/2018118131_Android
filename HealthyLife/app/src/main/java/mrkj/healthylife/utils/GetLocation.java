6f4da508b666a1cd701ab081642498a39db0eefa
Losia <1064972820@qq.com>
2020-12-19 14:00:44 +0800

    utils包-GetLocation类定义GetLocation()方法

--- HealthyLife/app/src/main/java/mrkj/healthylife/utils/GetLocation.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/utils/GetLocation.java
@@ -1,8 +1,24 @@
 package mrkj.healthylife.utils;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+
 /**
  * 位置信息配置
  */
 public class GetLocation {
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+
+    public GetLocation(Context context){
+        mLocationClient = new LocationClient(context);//声明LocationClient类
+        initLocation();
+        mLocationClient.registerLocationListener(myListener);
+        Log.e("执行了", "?");
+        mLocationClient.start();
+    }
 
 }
