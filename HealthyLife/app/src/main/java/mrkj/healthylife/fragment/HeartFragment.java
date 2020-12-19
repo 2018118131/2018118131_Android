a84462b6e03e571e5be9837bbc55f970095abb74
Losia <1064972820@qq.com>
2020-12-19 23:11:11 +0800

    fragment包-HeartFragment定义setCameraDisplayOrientation()方法根据横竖屏自动调节preview方向

--- HealthyLife/app/src/main/java/mrkj/healthylife/fragment/HeartFragment.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/fragment/HeartFragment.java
@@ -1,6 +1,7 @@
 package mrkj.healthylife.fragment;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -12,6 +13,7 @@ import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -219,6 +221,44 @@ public class HeartFragment extends BaseFragment implements View.OnClickListener 
         parameters.setPictureSize(640, 480);
         camera.setParameters(parameters);
 //        Toast.makeText(context,"开启摄像头执行了！",Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 根据横竖屏自动调节preview方向
+     *
+     * @param activity
+     * @param cameraId
+     * @param camera
+     */
+    private void setCameraDisplayOrientation(Activity activity, int cameraId, Camera camera) {
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;
+        } else {
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        if (camera != null) {
+            camera.setDisplayOrientation(result);
+        }
     }
 
     @Override
