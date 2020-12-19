5f6b82afe858a43c67a8b7aecef0793187bd4e21
Losia <1064972820@qq.com>
2020-12-19 18:35:46 +0800

    activity包-FoodHotListActivity重写initValues()方法初始化数据资源

--- HealthyLife/app/src/main/java/mrkj/healthylife/activity/FoodHotListActivity.java
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/FoodHotListActivity.java
@@ -1,13 +1,19 @@
 package mrkj.healthylife.activity;
 
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.widget.ExpandableListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import mrkj.healthylife.R;
 import mrkj.healthylife.base.BaseActivity;
+import mrkj.healthylife.entity.FoodMessage;
 import mrkj.healthylife.entity.FoodType;
+import mrkj.healthylife.utils.DBHelper;
 
 public class FoodHotListActivity extends BaseActivity {
 
@@ -39,9 +45,64 @@ public class FoodHotListActivity extends BaseActivity {
 
     }
 
+    /**
+     * 初始化数据资源
+     */
     @Override
     protected void initValues() {
+        ids = new int[]{R.mipmap.mrkj_gu, R.mipmap.mrkj_cai,
+                R.mipmap.mrkj_guo, R.mipmap.mrkj_rou, R.mipmap.mrkj_dan,
+                R.mipmap.mrkj_yv, R.mipmap.mrkj_nai, R.mipmap.mrkj_he,
+                R.mipmap.mrkj_jun, R.mipmap.you};
+        bitmaps = new Bitmap[ids.length];
+        for (int i = 0;i < ids.length ; i++){
+            bitmaps[i] = BitmapFactory.decodeResource(getResources(),ids[i]);
+        }
+        food_type_array = new String[]{"五谷类",
+                "蔬菜类", "水果类", "肉类",
+                "蛋类", "水产类", "奶类",
+                "饮料类", "菌藻类", "油脂类"};
+        food_list = new ArrayList<>();
+        //构造数据源
+        DBHelper dbHelper = new DBHelper();
+        Cursor cursor = dbHelper.selectAllDataOfTable("hot");
+//        Log.e("数据数量", cursor.getCount() + "");
+//        int j = 0;
+        for (int i = 0; i < 10; i++) {
+            FoodType foodType = null;
+            List<FoodMessage> foods = null;
+            int counts = 1;
+            while (cursor.moveToNext()) {
+//                Log.e("计数", (++j) + "");
+//                Log.e("counts", counts + "");
+                String name = cursor.getString(cursor.getColumnIndex("name"));
+                String hot = cursor.getString(cursor.getColumnIndex("hot"));
+                String type_name = cursor.getString(cursor.getColumnIndex("type_name"));
+                if (counts == 1) {
+                    foodType = new FoodType();
+                    foods = new ArrayList<>();
+                    foodType.setFood_type(type_name);
+//                    Log.e("type_name", type_name + "");
 
+                }
+                FoodMessage foodMessage = new FoodMessage();
+                foodMessage.setFood_name(name);
+                foodMessage.setHot(hot);
+                foods.add(foodMessage);
+                foodType.setFood_list(foods);
+                if (counts == 20) {
+                    food_list.add(foodType);
+                    break;
+                }
+                counts++;
+            }
+        }
+        cursor.close();
+        Log.e("数据的长度", food_list.size() + "");
+//        for (FoodType foodType : food_list) {
+//            Log.e("食物类型", foodType.getFood_type() + "");
+//            Log.e("食物数量",foodType.getFood_list().size() + "");
+//        }
     }
 
     @Override
