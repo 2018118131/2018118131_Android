87b42108f034a7e6c5195f88cfc71c3f52c8964b
Losia <1064972820@qq.com>
2020-12-19 20:00:22 +0800

    activity包-创建SettingHealthyHealthyActivity用于处理添加计划相关功能

---
+++ HealthyLife/app/src/main/java/mrkj/healthylife/activity/SettingHealthyHealthyActivity.java
@@ -0,0 +1,22 @@
+package mrkj.healthylife.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TimePicker;
+
+/**
+ * 添加计划的界面
+ */
+public class SettingHealthyHealthyActivity extends AppCompatActivity implements TimePicker.OnTimeChangedListener, View.OnClickListener {
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+
+    }
+}
